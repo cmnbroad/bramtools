a81240115bea3f9fcb5ac5a0c636a0dee61c207f
Chris Norman <cnorman@broadinstitute.org>
2020-11-22 11:18:03 -0500

    Refactor command line arg processing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use noodles_bam::{self as bam, bai};
 use noodles_sam as sam;
 use noodles::Region;
@@ -22,13 +24,39 @@ pub fn write_bam(in_file: &String, _out_file: &String) -> io::Result<()> {
 
 #[cfg(test)]
 mod test {
-    use crate::tools::write_bam;
 
     #[test]
     fn test_write_bam() {
+        use crate::write_bam;
+
         let in_file = String::from("testdata/example.bam");
         let out_file = String::from("testdata/out.bam");
         write_bam(&in_file, &out_file).expect("test failed")
     }
 
 }
+
+pub struct Bundle {
+    pub command: String,
+    pub input_file: String,
+    pub output_file: String
+}
+
+impl Bundle {
+    pub fn new(args: &[String]) -> Result<Bundle, &'static str> {
+        if args.len() < 4 {
+            return Err("Not enough arguments\nUsage: program_name command input_file output_file");
+        }
+        let command = args[1].clone();
+        let output_file = args[2].clone();
+        let input_file = args[3].clone();
+        Ok(Bundle { command, input_file, output_file })
+    }
+
+    pub fn run(&self) -> Result<(), Box<dyn Error>> {
+        println!("Command:{} Input:{} Output:{}\n", self.command, self.input_file, self.output_file);
+        write_bam(&self.input_file, &self.output_file)?;
+        Result::Ok(())
+    }
+}
+
